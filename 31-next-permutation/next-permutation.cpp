787bed321246dc69f0a695ce60c1f5cc7b4973e5
MananGhanshani <83908654+MananGhanshani@users.noreply.github.com>
2026-08-27 20:07:54 +0530

    Time: 0 ms (100.00%) | Memory: 15.9 MB (15.03%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int ind = -1;
-        int n = nums.size();
-        for (int i = n - 2; i >= 0 ; i--){
-            if (nums[i] < nums[i+1]){
-                ind = i;
-            break;
+        int  n = nums.size();
+        vector<int> sub;
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+        for (int i = n-1 ; i >= 0; i--){
+            if(nums[i] >= maxi){
+                sub.push_back(nums[i]);
+                maxi = max(maxi,nums[i]);
+            }
+            else{
+                for (int j = 0; j < sub.size(); j++ ){
+                    if (sub[j] > nums[i]){
+                        mini = min(mini,sub[j]);
+                    }
+                }
+                auto it = find(nums.begin()+i+1,nums.end(),mini);
+                swap(*it,nums[i]);
+                sort(nums.begin()+i+1,nums.end());
+                return;
             }
         }
-        if( ind == -1){
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-        for (int i = n-1; i > ind; i-- ){
-            if (nums[i] > nums[ind]){
-                swap(nums[i], nums[ind]);
-                break;
-            }
-        }
-        reverse(nums.begin() + ind + 1, nums.end());
-        return;
+        reverse(nums.begin(),nums.end());
     }
 };
